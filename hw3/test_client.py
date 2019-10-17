26481dbcea3530be1a832401e5dae143d76da8d7
alfred <njugzh@gmail.com>
2019-10-17 13:45:30 -0700

    finished testing client

--- hw3/test_client.py
+++ hw3/test_client.py
@@ -7,23 +7,22 @@ conn = http.client.HTTPConnection(WEB_API_SERVER, WEB_API_PORT)
 # test /api/gettime
 conn.request('GET', GET_TIME)
 r = conn.getresponse()
-print(r.status, r.reason)
 cache = r.read(int(r.headers.get(CONTENT_LENGTH_HEADER)))
-print(cache.decode('utf-8'))
+print("/api/gettime returns: {0}".format(cache.decode('utf-8')))
 time.sleep(3)
 
-
-body = '1+2+3+6'
-conn.request('POST', EVAL_EXPRESSION, body)
-r = conn.getresponse()
-print(r.status, r.reason)
-cache = r.read(int(r.headers.get(CONTENT_LENGTH_HEADER)))
-print(cache.decode('utf-8'))
-time.sleep(3)
+# test /api/evalexpression
+expressions = ['1+2+3+6', '23-145+1+1920', '77+218398139-221']
+for i in range(len(expressions)):
+    conn.request('POST', EVAL_EXPRESSION, expressions[i])
+    r = conn.getresponse()
+    cache = r.read(int(r.headers.get(CONTENT_LENGTH_HEADER)))
+    print(expressions[i], " = ", cache.decode('utf-8'))
+    time.sleep(3)
 
 # test /html.status
 conn.request('GET', STATUS)
 r = conn.getresponse()
-print(r.status, r.reason)
+print('/status.html returns the following html:')
 cache = r.read(int(r.headers.get('Content-Length')))
 print(cache.decode('utf-8'))
