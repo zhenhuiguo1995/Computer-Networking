ea323901f97b5942cb2c7cbcb39f32827c0a545c
alfred <njugzh@gmail.com>
2019-12-02 12:04:40 -0800

    reformatted file

--- assignment6_zhenhui_guo/util.py
+++ assignment6_zhenhui_guo/util.py
@@ -4,19 +4,19 @@ import threading
 # Convenient class to run a function periodically in a separate
 # thread.
 class PeriodicClosure:
-  def __init__(self, handler, interval_sec):
-    self._handler = handler
-    self._interval_sec = interval_sec
-    self._timer = None
+    def __init__(self, handler, interval_sec):
+        self._handler = handler
+        self._interval_sec = interval_sec
+        self._timer = None
 
-  def _timeout_handler(self):
-    self._handler()
-    self.start()
+    def _timeout_handler(self):
+        self._handler()
+        self.start()
 
-  def start(self):
-    self._timer = threading.Timer(self._interval_sec, self._timeout_handler)
-    self._timer.start()
+    def start(self):
+        self._timer = threading.Timer(self._interval_sec, self._timeout_handler)
+        self._timer.start()
 
-  def stop(self):
-    if self._timer:
-      self._timer.cancel()
+    def stop(self):
+        if self._timer:
+            self._timer.cancel()
