8118302d030508d1aa6af50f6b680a6040d4d948
alfred <njugzh@gmail.com>
2019-12-02 11:27:50 -0800

    inital commit

---
+++ assignment6_zhenhui_guo/router.py
@@ -0,0 +1,55 @@
+import os.path
+import socket
+import table
+import threading
+import util
+
+_CONFIG_UPDATE_INTERVAL_SEC = 5
+
+_MAX_UPDATE_MSG_SIZE = 1024
+_BASE_ID = 8000
+
+def _ToPort(router_id):
+  return _BASE_ID + router_id
+
+def _ToRouterId(port):
+  return port - _BASE_ID
+
+
+class Router:
+  def __init__(self, config_filename):
+    # ForwardingTable has 3 columns (DestinationId,NextHop,Cost). It's
+    # threadsafe.
+    self._forwarding_table = table.ForwardingTable()
+    # Config file has router_id, neighbors, and link cost to reach
+    # them.
+    self._config_filename = config_filename
+    self._router_id = None
+    # Socket used to send/recv update messages (using UDP).
+    self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+  def start(self):
+    # Start a periodic closure to update config.
+    self._config_updater = util.PeriodicClosure(
+        self.load_config, _CONFIG_UPDATE_INTERVAL_SEC)
+    self._config_updater.start()
+    # TODO: init and start other threads.
+    while True: pass
+
+
+  def stop(self):
+    if self._config_updater:
+      self._config_updater.stop()
+    # TODO: clean up other threads.
+
+
+  def load_config(self):
+    assert os.path.isfile(self._config_filename)
+    with open(self._config_filename, 'r') as f:
+      router_id = int(f.readline().strip())
+      # Only set router_id when first initialize.
+      if not self._router_id:
+        self._socket.bind(('localhost', _ToPort(router_id)))
+        self._router_id = router_id
+      # TODO: read and update neighbor link cost info.
